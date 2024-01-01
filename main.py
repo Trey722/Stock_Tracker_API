387dea6509b7bd74f81f31d323789b64fdda6013
RougeAce <127271451+RougeAce@users.noreply.github.com>
2023-12-31 19:40:41 -0600

    The first version

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-import getPrice
+import get_data
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -8,13 +9,39 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/stock_price/{ticker}")
+
+@app.get("stock/data/stock_price/{ticker}")
 async def getPriceStock(ticker: str):
     try:
-        price = getPrice.get_stock_price(ticker)
+        price = get_data.get_stock_price(ticker)
         if isinstance(price, float):
             return {ticker: price}
         else:
-            return {"error": f"Failed to fetch price for {ticker}"}
+            return {"status": 502,"error": f"Failed to fetch price for {ticker}"}
     except Exception as e:
-        return {"error": str(e)}
+        return {"status": 500,"error": str(e)}
+    
+    
+    
+@app.get("stock/data/options_chain/{ticker}/{date}")
+async def get_options_chain_endpoint(ticker: str, date):
+    try:
+        date = str(date)
+        options_data = get_data.get_options_chain(ticker, date)
+        return options_data
+    except Exception as e:
+        return {"status": 500, "error": str(e)}
+    
+    
+@app.get("/stock/data/balanace_sheet/{ticker}")
+async def get_balanace_sheet_endpoint(ticker):
+    try:
+        return get_data.get_balanace_sheet(ticker)
+    except Exception as e:
+        return {"status": 500, "error": str(e)}
+
+        
+        
+
+    
+    
